87a36e76009f8d3d272ad0ca4bedf37b261ddcbd
Anusha0626 <52437606+Anusha0626@users.noreply.github.com>
2025-07-20 17:29:31 +0530

    modify script file

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,6 +1,5 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f elephant "I AM ELEPHANT" >> dragon.txt
-grep -i "elephant" dragon.txt
-cat dragon.txt
-ls -ltra
+cowsay -f elephant "I AM ELEPHANT" >> elephant.txt
+cat elephant.txt
+ls ltra
